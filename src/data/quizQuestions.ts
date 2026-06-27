81574e45e43ee0ac1491ce434729d95034e75a72
shinee <shineetejolpatro@gmail.com>
2026-06-27 11:55:39 +0530

    feat: add sustainability quiz generator

---
+++ src/data/quizQuestions.ts
@@ -0,0 +1,13 @@
+export const quizQuestions = [
+  {
+    category: "climate",
+    question: "Which gas contributes most to global warming?",
+    options: [
+      "Oxygen",
+      "Nitrogen",
+      "Carbon Dioxide",
+      "Hydrogen"
+    ],
+    answer: "Carbon Dioxide"
+  }
+];
